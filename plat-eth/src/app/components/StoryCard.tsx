9ff8808c5aaff38f8a12bb66d8dd57302870f896
Aye Chan San Tun <ayechan@Ayes-Mac-mini.local>
2024-03-06 22:43:37 -0800

    first component

---
+++ plat-eth/src/app/components/StoryCard.tsx
@@ -0,0 +1,6 @@
+const StoryCard = () => {
+    return (
+        <div>this is a story card</div>
+    )
+}
+export default StoryCard;
---
+++ plat-eth/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { DEV_MIDDLEWARE_MANIFEST } from "next/dist/shared/lib/constants";
+import Image from "next/image";
+import StoryCard from "./components/StoryCard";
+
+export default function Home() {
+  return (
+    <>
+    <div>hello</div>
+    <StoryCard />
+    </>
+  );
+}
